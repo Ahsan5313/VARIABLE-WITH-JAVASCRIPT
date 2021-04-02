c85357bcbc9720f67ce29453ae9305c5b532ac08
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-02 09:35:00 +0600

    add html and javascript for  the project

---
+++ index.js
@@ -0,0 +1,9 @@
+var thisIsFaristName ="ahsanul"
+var thisIslastName ="huq"
+var thisIsAge ="21"
+var thisIsClass ="honurs"
+
+document.write(thisIsFaristName);
+document.write(thisIslastName);
+document.write(thisIsAge);
+document.write(thisIsClass);
